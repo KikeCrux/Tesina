ed2b7c5461d8f22db2c909f831f602a64b6e4007
KikeCrux <kikke8508@gmail.com>
2024-10-04 22:19:54 -0600

    fix: Consulta a la BD de los productos

--- Backend/NodeJs/index.js
+++ Backend/NodeJs/index.js
@@ -70,20 +70,27 @@ app.get('/productos', (req, res) => {
     const { id_usuario, tipo_usuario } = req.query;
 
     let query = '';
+    let queryParams = [];
 
-    if (tipo_usuario === 'mayoreo') {
+    // Condición para mostrar todos los productos si no hay id_usuario
+    if (!id_usuario) {
         query = `
-      SELECT p.id_producto, p.nombre, pm.precio AS precio, p.imagen_url 
-      FROM productos p 
-      JOIN preciosmayoreo pm ON p.id_producto = pm.id_producto 
-      WHERE pm.id_usuario = ?`;
+        SELECT id_producto, nombre, precio_menudeo AS precio, imagen_url 
+        FROM productos`;
+    } else if (tipo_usuario === 'mayoreo') {
+        query = `
+        SELECT p.id_producto, p.nombre, pm.precio AS precio, p.imagen_url 
+        FROM productos p 
+        JOIN preciosmayoreo pm ON p.id_producto = pm.id_producto 
+        WHERE pm.id_usuario = ?`;
+        queryParams.push(id_usuario);
     } else {
         query = `
-      SELECT id_producto, nombre, precio_menudeo AS precio, imagen_url 
-      FROM productos`;
+        SELECT id_producto, nombre, precio_menudeo AS precio, imagen_url 
+        FROM productos`;
     }
 
-    db.query(query, [id_usuario], (err, results) => {
+    db.query(query, queryParams, (err, results) => {
         if (err) {
             return res.status(500).json({ message: 'Error al obtener los productos', error: err });
         }
