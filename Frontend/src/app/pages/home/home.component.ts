1ffd74e17c937531845be573ccc01ad01b5fc76e
Jacqueline-Renito <al291218@edu.uaa.mx>
2024-11-30 21:33:33 -0600

    Ajuste Navbar

--- Frontend/src/app/pages/home/home.component.ts
+++ Frontend/src/app/pages/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CarouselComponent } from '../../components/carousel/carousel.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarouselComponent],
+  imports: [CarouselComponent, HeaderComponent, FooterComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
